92674376c112dba512c75c0d7341384a6a32379c
Deadly mind <oussamaayari2014@gmail.com>
2023-11-08 19:29:50 +0100

    add author to models

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 
 '''
         1 : html widget
@@ -22,6 +23,7 @@ from taggit.managers import TaggableManager
 '''
 
 class Post(models.Model):
+    author = models.ForeignKey(User,related_name='post_author', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=20000)
     draft = models.BooleanField(default=True)

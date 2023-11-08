8e733e7b8cbe97990a2e3e3aa077add78d5597a6
Deadly mind <oussamaayari2014@gmail.com>
2023-11-08 17:28:39 +0100

    add publish date

--- posts/admin.py
+++ posts/admin.py
@@ -8,5 +8,6 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'draft']
     list_filter = ['draft']
     search_fields = ['title']
+    
 
 admin.site.register(Post,ProductAdmin)
--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 '''
         1 : html widget
         2 : validation
@@ -23,6 +23,8 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=20000)
     draft = models.BooleanField(default=True)
+    publish_date = models.DateTimeField(default=timezone.now)
+    # publish_date2 = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title

22abf26974cd68f3da192ddacccaf73648765fb5
Deadly mind <oussamaayari2014@gmail.com>
2023-11-08 02:13:41 +0100

    design post models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+'''
+        1 : html widget
+        2 : validation
+        3 : best for db
+'''
+
+'''
+    post:
+        - title
+        - content
+        - draft
+        - publish_date
+        - autor
+        - images
+        - tags
+        - category
+        - comments
+'''
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=20000)
+    draft = models.BooleanField(default=True)
+
+

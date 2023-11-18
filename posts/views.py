7c9203b3c3cfc51303922604510bf8620a9cb512
Deadly mind <oussamaayari2014@gmail.com>
2023-11-18 19:42:57 +0100

    add delete post

--- posts/views.py
+++ posts/views.py
@@ -71,6 +71,17 @@ def edit_post(request,pk):
     return render(request, 'posts/edit.html', {'form':form})
 
 
+def delete_post(request, pk):
+
+    post = Post.objects.get(id=pk)
+    post.delete()
+    return redirect('/posts/')
+
+
+
+
+
+
 
 
 
--- project/urls.py
+++ project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
-from posts.views import post_list, post_details, PostList, PostDetail, create_post, edit_post
+from posts.views import post_list, post_details, PostList, PostDetail, create_post, edit_post, delete_post
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +26,7 @@ urlpatterns = [
     path('posts/new', create_post),
     path('posts/<int:pk>', PostDetail.as_view()),
     path('posts/<int:pk>/edit', edit_post),
+    path('posts/<int:pk>/delete', delete_post),
     path('summernote/', include('django_summernote.urls')),
 
 ]
